d443c2d5cdee33484f1e8c5a3e1010e7361027bd
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2024-02-19 23:04:26 +0300

    change code for google auth

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { FC} from "react";
 import { CredentialResponse, GoogleLogin} from '@react-oauth/google';
+import axios from "axios";
 
 interface LoginProps {
 	setIsLoggedIn: (value: boolean) => void;
@@ -8,31 +9,28 @@ interface LoginProps {
 
 export const LoginPage: FC<LoginProps>  = ({setIsLoggedIn}) => {
 
-	const handleSuccess = (credentialResponse: CredentialResponse) => {
+	const handleSuccess = async (credentialResponse: CredentialResponse) => {
+		console.log(credentialResponse);
 		// If you are using the authorization code flow, you will receive a code to be exchanged for an access token
-		const authorizationCode = credentialResponse.code;
+		const authorizationCode = credentialResponse.credential;
   
-		// Send the authorization code to your backend server
-		fetch('/api/auth/google', {
-		  method: 'POST',
-		  headers: {
-			 'Content-Type': 'application/json',
-		  },
-		  body: JSON.stringify({ code: authorizationCode }),
-		})
-		.then(response => response.json())
-		.then(data => {
-		  // Handle the response from your backend server
-		  console.log('Login successful, backend response:', data);
-		})
-		.catch(error => {
-		  // Handle errors in communicating with your backend server
-		  console.error('Error exchanging authorization code:', error);
-		});
-	 };
-  
-	 const handleError = (errorResponse: any) => {
-		console.error('Google login failed', errorResponse);
+		try {
+			 // Send the authorization code to your backend server
+			 const response = await axios.post('/api/auth/google', { code: authorizationCode }, {
+				  headers: {
+						'Content-Type': 'application/json',
+						'Cross-Origin-Opener-Policy': 'no-referrer-when-downgrade'
+				  }
+			 });
+			 // Handle the response from your backend server
+			 console.log('Login successful, backend response:', response.data);
+		} catch (error) {
+			 // Handle errors in communicating with your backend server
+			 console.error('Error exchanging authorization code:', error);
+		}
+  };
+	 const handleError = () => {
+		console.error('Google login failed');
 	 };
 
 
@@ -43,7 +41,6 @@ export const LoginPage: FC<LoginProps>  = ({setIsLoggedIn}) => {
         onSuccess={handleSuccess}
         onError={handleError}
         useOneTap
-        flow="auth-code"
       />
  </>
 };
